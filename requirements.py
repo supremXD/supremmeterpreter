bdc0747a6446c6df5e4d55e2714751a79d549acc
supremXD <74043675+supremXD@users.noreply.github.com>
2021-11-06 01:13:30 +0100

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,13 +1,14 @@
 import os, time
 
-print("1 -->> Arch")
-print("2 -->> Debian (Kali Linux)")
+
+print("1 -->> Debian (Kali Linux)")
+print("2 -->> Arch")
 print("")
 x = input("-->> ")
 
 if x == "1":
     print("Downloading...")
-    os.system("sudo pacman -S git bash xterm --noconfirm")
+    os.system("sudo apt install git bash xterm -y")
     time.sleep(1)
     print("Done!!")
     time.sleep(2)
@@ -15,7 +16,7 @@ if x == "1":
 
 if x == "2":
     print("Downloading...")
-    os.system("sudo apt install git bash xterm -y")
+    os.system("sudo pacman -S git bash xterm --noconfirm")
     time.sleep(1)
     print("Done!!")
     time.sleep(2)
